5f7b24513a90e60ba9963de63ed71d1b63b11058
hw_008618923830216_01/hp <Cld@2021>
2023-06-25 11:38:29 +0800

    运行测试用力模块

--- Run.py
+++ Run.py
@@ -25,8 +25,8 @@ if __name__ == '__main__':
     # 执行测试用例生成测试数据，如果已经存在报告，那就先清空，然后再生成新的测试报告，使用命令： --clean-alluredir
         pytest.main(['-vs', test, '--clean-alluredir', '--alluredir', './allure-results'])
     if test01 == 1 :   # 1 是生产环境
-        test="./Test_casa/test_ict_long.py"
-        # test = ".//Test_casa"
+        # test="./Test_casa/test_ict_long.py"
+        test = ".//Test_casa"
     # 执行测试用例生成测试数据，如果已经存在报告，那就先清空，然后再生成新的测试报告，使用命令： --clean-alluredir
         pytest.main(['-vs', test, '--clean-alluredir', '--alluredir', './allure-results'])
 
