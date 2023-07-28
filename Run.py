c9b27793b37be5b00c3617adb81cc37ee2f0075f
hp <980ec089c44b4278bedc6d09cbfeb34b@example.com>
2023-07-28 15:30:51 +0800

    更新 Run.py

--- Run.py
+++ Run.py
@@ -6,4 +6,4 @@ if __name__ == '__main__':
     # test="./Test_casa/test_ict_long.py"
     test = ".//testcases"
     # 执行测试用例生成测试数据，如果已经存在报告，那就先清空，然后再生成新的测试报告，使用命令： --clean-alluredir
-    pytest.main(['-vs', test, '--clean-alluredir', '--alluredir', './allure-results'])
+    pytest.main(['-vs', test, '--clean-alluredir', '--alluredir', './report'])
