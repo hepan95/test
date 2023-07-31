af9dc9eb3b833e87273409c409aa9c5a04651a11
hp <980ec089c44b4278bedc6d09cbfeb34b@example.com>
2023-07-31 13:40:05 +0800

    删除

--- Run.py
+++
@@ -1,5 +0,0 @@
-import pytest
-
-if __name__ == '__main__':
-    # 执行测试用例生成测试数据，如果已经存在报告，那就先清空，然后再生成新的测试报告，使用命令： --clean-alluredir
-    pytest.main(['-vs', '--clean-alluredir', '--alluredir', './report'])
